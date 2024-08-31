88158a4758153d605d5de1c0e3d00820a27bcb6d
Kayky Matos Santana <kayky7277@gmail.com>
2024-08-31 16:17:30 -0300

    Maui app started, new front-end project

---
+++ src/Frontend/Mobile/University.Manager.Project.Mobile.MauiAppUniversity/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace University.Manager.Project.Mobile.MauiAppUniversity
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ src/Frontend/Mobile/University.Manager.Project.Mobile.MauiAppUniversity/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace University.Manager.Project.Mobile.MauiAppUniversity
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
