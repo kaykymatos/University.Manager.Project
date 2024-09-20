18bc4700e1cffdf0b2920c9a37ee8d7c7a3cb5eb
Kayky Matos Santana <kayky7277@gmail.com>
2024-09-20 14:50:08 -0300

    basic Course and course category view

--- src/Frontend/Mobile/University.Manager.Project.Mobile.MauiAppUniversity/Views/Actions/Courses.xaml.cs
+++ src/Frontend/Mobile/University.Manager.Project.Mobile.MauiAppUniversity/Views/Actions/Courses.xaml.cs
@@ -1,3 +1,5 @@
+using University.Manager.Project.Mobile.MauiAppUniversity.Models;
+
 namespace University.Manager.Project.Mobile.MauiAppUniversity.Views.Actions;
 
 public partial class Courses : ContentPage
@@ -5,5 +7,38 @@ public partial class Courses : ContentPage
     public Courses()
     {
         InitializeComponent();
+        var list = new List<CourseViewModel>
+        {
+            new CourseViewModel
+            {
+                Name="aa",
+                Description="teste",
+                CourseCategoryId=1,
+                CreationData=DateTime.Now,
+                CourseCategory=new CourseCategoryViewModel
+                {
+                    Name="teste"
+                },
+                TotalValue=1000,
+                UpdatedData=DateTime.Now,
+                Workload=100
+            },
+            new CourseViewModel
+            {
+                 Name="aa2",
+                Description="teste2",
+                CourseCategoryId=1,
+                CreationData=DateTime.Now,
+                CourseCategory=new CourseCategoryViewModel
+                {
+                    Name="teste"
+                },
+                TotalValue=1000,
+                UpdatedData=DateTime.Now,
+                Workload=100
+            },
+        };
+
+        listView.ItemsSource = list;
     }
 }
